b341dd64a1de725db42d2eb6b615935ae5eb3f7e
Lilian Chardon <55657944+Tequhella@users.noreply.github.com>
2022-03-05 20:17:25 +0100

    Update Led_x3_clignotement.asm

--- Led_x3_clignotement/Led_x3_clignotement.asm
+++ Led_x3_clignotement/Led_x3_clignotement.asm
@@ -7,9 +7,9 @@
 
 
 start:
-	ldi		r17, (1<<DDB2)|(1<<DDB1)|(1<<DDB0)
-    out		DDRB, r17
-	clr		r17
+	ldi		r17, (1<<DDB2)|(1<<DDB1)|(1<<DDB0)	; Chargement du registre r17 avec les pins 0, 1 et 2 dÃ©finit Ã  1
+    out		DDRB, r17							; Pin 0, 1 et 2 dÃ©finit comme sorties
+	clr		r17									; Registre vidÃ©
 
 boucle:
 	sbi		PORTB, PORTB0	; Pin 0 : tension ON
@@ -29,8 +29,8 @@ tempo:
 deltempo1:
 	ldi		r17, 0xff
 deltempo2:
-	dec		r17					; Décrémentation du registre r17
-	brbc	SREG_Z, deltempo2	; Si le flag n'est pas vidé, alors retour à la sous routine deltempo2
-	dec		r16					; Décrémentation du registre r16
-	brbc	SREG_Z, deltempo1	; Si le flag n'est pas vidé, alors retour à la sous routine deltempo1
+	dec		r17					; DÃ©crÃ©mentation du registre r17
+	brbc	SREG_Z, deltempo2	; Si le flag n'est pas vidÃ©, alors retour Ã  la sous routine deltempo2
+	dec		r16					; DÃ©crÃ©mentation du registre r16
+	brbc	SREG_Z, deltempo1	; Si le flag n'est pas vidÃ©, alors retour Ã  la sous routine deltempo1
 	ret							; Sortie de la sous routine
