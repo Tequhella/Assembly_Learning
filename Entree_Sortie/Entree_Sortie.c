216b203f19bc96bc3c5e9fbab00ce21d0858e43b
Lilian Chardon <55657944+Tequhella@users.noreply.github.com>
2022-03-14 15:58:37 +0100

    Programme Entree_Sortie avec boutton

---
+++ Entree_Sortie/Entree_Sortie.c
@@ -0,0 +1,25 @@
+/*
+ * EntreeSortie.c
+ *
+ * Created: 14/03/2022 11:20:38
+ * Author : Tequhella
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	DDRB  |= (1 << DDB0);
+	DDRB  &= ~(1 << DDB1);
+	PORTB |= (1 << PB1);
+    
+    while (1)
+    {
+		if (PINB & (1 << PB1))
+			PORTB &= ~(1 << PB0);
+		else
+			PORTB |= (1 << PB0);
+    }
+}
+
