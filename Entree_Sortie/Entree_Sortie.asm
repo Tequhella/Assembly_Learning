9b1065eab646c4e45d544a299430bd18ce6fdffe
Lilian Chardon <55657944+Tequhella@users.noreply.github.com>
2022-08-03 11:02:40 +0100

    Update Entree_Sortie.asm
    Je passe de 3 instructions à 1 seule pour gérer l'entrée.

--- Entree_Sortie/Entree_Sortie.asm
+++ Entree_Sortie/Entree_Sortie.asm
@@ -6,19 +6,17 @@
 ;
 
 start:
-	sbi		DDRB, DDB0		; Pin 0 définit en tant que sortie
-	sbi		PORTB, PORTB1	; Résistance de tirage activé
+	sbi		DDRB, DDB0		; Pin 0 dÃ©finit en tant que sortie
+	sbi		PORTB, PORTB1	; RÃ©sistance de tirage activÃ©
 
 boucle:
-	in		r17, PINB		; Registre 17 prend la valeur définit de l'entrée
-	sbr		r16, (1 << PB1) ; Registre 16 prend la valeur obtenue en entrée
-	and		r16, r17		; Comparaison entre r16 et r17
-	brbc	SREG_Z, ledOFF	; Si la comparaison est vraie, alors on entre dans la sous routine ledOFF
-	sbis	PORTB, PORTB0	; Sinon, allumer la led si elle est éteinte
+	sbic	PINB, PINB1		; Comparaison entre r16 et r17
+	rcall	ledOFF			; Si la comparaison est vraie, alors on entre dans la sous routine ledOFF
+	sbis	PORTB, PORTB0	; Sinon, allumer la led si elle est Ã©teinte
 	sbi		PORTB, PORTB0	; Allume la led
 	rjmp	boucle			; Retour de boucle
 
 ledOFF:
-	sbic	PORTB, PORTB0	; Si la led est éteinte, ignorer l'instruction suivante
+	sbic	PORTB, PORTB0	; Si la led est Ã©teinte, ignorer l'instruction suivante
 	cbi		PORTB, PORTB0	; Eteindre la led
 	rjmp	boucle			; Retour dans la boucle
