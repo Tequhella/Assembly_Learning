2b152e3d00e0d96c5d99fa4ac9b798c0891c4fe3
Lilian Chardon <55657944+Tequhella@users.noreply.github.com>
2022-08-03 15:04:54 +0100

    Réorganisation du code en assembleur Entree_Sortie

--- Entree_Sortie/Entree_Sortie.asm
+++ Entree_Sortie/Entree_Sortie.asm
@@ -9,14 +9,21 @@ start:
 	sbi	DDRB, DDB0	; Pin 0 définit en tant que sortie
 	sbi	PORTB, PORTB1	; Résistance de tirage activé
 
+; Boucle principale
 boucle:
-	sbic	PINB, PINB1	; Vérifie si l'état du PIN1 est à 0
-	rcall	ledOFF		; Si la comparaison est vraie, alors on entre dans la sous routine ledOFF
-	sbis	PORTB, PORTB0	; Sinon, allumer la led si elle est éteinte
-	sbi	PORTB, PORTB0	; Allume la led
+	sbic	PINB, PINB1	; Vérifie si l'état du pin 1 est à  0
+	rcall	ledOFF		; Appelle de la sous routine ledOFF
+	rcall	ledON		; Appelle de la sous routine ledON
 	rjmp	boucle		; Retour de boucle
 
+; Sous-routine ledOFF : Désactive la LED
 ledOFF:
-	sbic	PORTB, PORTB0	; Si la led est éteinte, ignorer l'instruction suivante
+	sbic	PORTB, PORTB0	; Si la led est éteinte, ignorer l'instruction extinction
 	cbi	PORTB, PORTB0	; Eteindre la led
-	ret			; Retour dans la boucle
+	rjmp	boucle		; Retour dans la boucle
+
+; Sous-routine ledON : Active la LED
+ledON:
+	sbis	PORTB, PORTB0	; Si la led est allumé, ignorer l'instruction d'allumage
+	sbi	PORTB, PORTB0	; Allume la led
+	ret			; retour dans la boucle
