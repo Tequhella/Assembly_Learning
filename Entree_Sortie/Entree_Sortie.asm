e99ff86820da26e709b0e3518c664ca3ac1c6ea1
Lilian Chardon <55657944+Tequhella@users.noreply.github.com>
2022-08-03 11:11:20 +0100

    Update Entree_Sortie.asm

--- Entree_Sortie/Entree_Sortie.asm
+++ Entree_Sortie/Entree_Sortie.asm
@@ -6,17 +6,17 @@
 ;
 
 start:
-	sbi		DDRB, DDB0		; Pin 0 définit en tant que sortie
-	sbi		PORTB, PORTB1	; Résistance de tirage activé
+	sbi	DDRB, DDB0	; Pin 0 définit en tant que sortie
+	sbi	PORTB, PORTB1	; Résistance de tirage activé
 
 boucle:
-	sbic	PINB, PINB1		; Comparaison entre r16 et r17
-	rcall	ledOFF			; Si la comparaison est vraie, alors on entre dans la sous routine ledOFF
+	sbic	PINB, PINB1	; Vérifie si l'état du PIN1 est à 0
+	rcall	ledOFF		; Si la comparaison est vraie, alors on entre dans la sous routine ledOFF
 	sbis	PORTB, PORTB0	; Sinon, allumer la led si elle est éteinte
-	sbi		PORTB, PORTB0	; Allume la led
-	rjmp	boucle			; Retour de boucle
+	sbi	PORTB, PORTB0	; Allume la led
+	rjmp	boucle		; Retour de boucle
 
 ledOFF:
 	sbic	PORTB, PORTB0	; Si la led est éteinte, ignorer l'instruction suivante
-	cbi		PORTB, PORTB0	; Eteindre la led
-	rjmp	boucle			; Retour dans la boucle
+	cbi	PORTB, PORTB0	; Eteindre la led
+	ret			; Retour dans la boucle
