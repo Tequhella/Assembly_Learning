091e0787a4775db8d6cb759c6b343e64e01dc9bb
Lilian Chardon <55657944+Tequhella@users.noreply.github.com>
2023-12-07 12:46:04 +0100

    Ajout makefile + optimisation code Entree_Sortie

--- Entree_Sortie/Entree_Sortie.asm
+++ Entree_Sortie/Entree_Sortie.asm
@@ -5,25 +5,35 @@
 ; Author : Tequhella
 ;
 
+.DEVICE ATTiny25
+
+.equ PORTB = 0x18
+.equ DDRB = 0x17
+.equ PINB = 0x16
+
+.equ PORTB0 = 0
+.equ PORTB1 = 1
+.equ DDB0 = 0
+.equ PINB1 = 1
+
 start:
-	sbi	DDRB, DDB0	; Pin 0 définit en tant que sortie
-	sbi	PORTB, PORTB1	; Résistance de tirage activé
+    sbi	DDRB, DDB0	    ; Pin 0 définit en tant que sortie
+    sbi	PORTB, PORTB1	; Résistance de tirage activé
 
 ; Boucle principale
 boucle:
-	sbic	PINB, PINB1	; Vérifie si l'état du pin 1 est à  0
-	rcall	ledOFF		; Appelle de la sous routine ledOFF
-	rcall	ledON		; Appelle de la sous routine ledON
-	rjmp	boucle		; Retour de boucle
+    sbic	PINB, PINB1	; Vérifie si l'état du pin 1 est à  0
+    rcall	ledON		; Appelle de la sous routine ledON
+    sbis	PINB, PINB1	; Vérifie si l'état du pin 1 est à  1
+    rcall	ledOFF		; Appelle de la sous routine ledOFF
+    rjmp	boucle		; Retour de boucle
 
 ; Sous-routine ledOFF : Désactive la LED
 ledOFF:
-	sbic	PORTB, PORTB0	; Si la led est éteinte, ignorer l'instruction extinction
-	cbi	PORTB, PORTB0	; Eteindre la led
-	rjmp	boucle		; Retour dans la boucle
+    cbi	    PORTB, PORTB0	; Eteindre la led
+    ret			            ; retour dans la boucle
 
 ; Sous-routine ledON : Active la LED
 ledON:
-	sbis	PORTB, PORTB0	; Si la led est allumé, ignorer l'instruction d'allumage
-	sbi	PORTB, PORTB0	; Allume la led
-	ret			; retour dans la boucle
+    sbi	    PORTB, PORTB0	; Allume la led
+    ret			            ; retour dans la boucle
